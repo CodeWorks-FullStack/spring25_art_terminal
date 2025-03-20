58234725409a0c4f395582b87ff2e33c7f0562ae
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-20 09:43:24 -0600

    rendering projects as raw JSON

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,11 @@
 <script setup>
+import { AppState } from '@/AppState.js';
 import { projectsService } from '@/services/ProjectsService.js';
 import { logger } from '@/utils/Logger.js';
 import { Pop } from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
+
+const projects = computed(() => AppState.projects)
 
 onMounted(() => {
   getProjects()
@@ -19,7 +22,7 @@ async function getProjects() {
 </script>
 
 <template>
-  sup
+  {{ projects }}
 </template>
 
 <style scoped lang="scss"></style>
--- src/services/ProjectsService.js
+++ src/services/ProjectsService.js
@@ -1,10 +1,14 @@
 import { logger } from "@/utils/Logger.js"
 import { api } from "./AxiosService.js"
+import { Project } from "@/models/Project.js"
+import { AppState } from "@/AppState.js"
 
 class ProjectsService {
   async getProjects() {
     const response = await api.get('api/projects')
     logger.log('GOT PROJECTS', response.data)
+    const projects = response.data.map(pojo => new Project(pojo))
+    AppState.projects = projects
   }
 }
 
