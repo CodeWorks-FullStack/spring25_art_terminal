45f47124bc30bee9b03a0d4e1aae2a1b87fe1694
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-20 09:30:48 -0600

    got projects

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,21 @@
 <script setup>
+import { projectsService } from '@/services/ProjectsService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  getProjects()
+})
 
+async function getProjects() {
+  try {
+    await projectsService.getProjects()
+  } catch (error) {
+    Pop.error(error, 'Could not get projects')
+    logger.error('COULD NOT GET PROJECTS', error)
+  }
+}
 </script>
 
 <template>
