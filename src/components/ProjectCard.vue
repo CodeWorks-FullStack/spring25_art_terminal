f8430904d44f34c8491ea3e060075f9d5eed065f
Jeremy Fowler <jembyfowler@gmail.com>
2025-03-20 10:08:19 -0600

    added button and img count

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,12 +10,33 @@ defineProps({
 <template>
   <div class="card mb-3">
     <img :src="projectProp.coverImg" class="card-img-top" :alt="`Cover image for the ${projectProp.title} project`">
-    <div class="card-body">
-      <p class="card-title fs-5">{{ projectProp.title }}</p>
-      <p class="card-text"></p>
+    <div class="card-body d-flex justify-content-between align-items-center">
+      <div>
+        <p class="card-title fs-5">{{ projectProp.title }}</p>
+        <div class="card-text">
+          <p>Images: {{ projectProp.projectImgs.length }}</p>
+          <button class="btn btn-indigo">
+            Browse Images
+          </button>
+        </div>
+      </div>
+      <img :src="projectProp.creator.picture" :alt="projectProp.creator.name" class="creator-img"
+        :title="projectProp.creator.name">
     </div>
   </div>
 </template>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.creator-img {
+  height: 6em;
+  aspect-ratio: 1/1;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+.card-img-top {
+  height: 40dvh;
+  object-fit: cover;
+}
+</style>
